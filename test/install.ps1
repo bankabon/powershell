4dd8dd9eb061195e0bbcb42a993475fb0512e3a0
bankabon <kota.1447@gmail.com>
2023-09-19 00:02:13 +0900

    テスト実装

--- test/install.ps1
+++ test/install.ps1
@@ -3,9 +3,17 @@
 $filepass = "\\10.0.0.200\setup\installer"
 $installer = "C:\kitting"
 
-#
+#ディレクトリ作成
+mkdir C:\kitting
 
 #インストールファイル移行
 Copy-Item $filepass -Destination $installer
 
+#googlechromeサイレントインストール
+Start-Process -pass "$(installer)\googlechromestandaloneenterprise64.msi" -ArgumentList "/VERYSILENT /NORESTART" -Wait
 
+#vscodeサイレントインストール
+Start-Process -pass "$(installer)\VSCodeUserSetup-x64-1.82.0.exe" -ArgumentList "/VERYSILENT /NORESTART" -Wait
+
+#terratermサイレントインストール
+Start-Process -pass "$(installer)\WinSCP-6.1.1-Setup.exe" -ArgumentList "/VERYSILENT /NORESTART" -Wait
